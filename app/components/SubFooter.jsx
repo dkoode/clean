41c3aa459627d6ed87122b6f6000a0954eea268a
Doganay KOSEOGLU <81515348+dkoode@users.noreply.github.com>
2024-11-05 20:12:13 +0300

    phone added footer

--- app/components/SubFooter.jsx
+++ app/components/SubFooter.jsx
@@ -8,8 +8,8 @@ export default function SubFooter() {
                     İhtiyaçlarınıza yönelik en uygun hizmeti almak için bizim iletişime geçin!
                 </h5>
                 <div className="flex w-full flex-col md:flex-row  space-y-3 md:space-y-0 md:gap-x-5 items-center">
-                    <Link href="#" className="py-4 px-6 rounded-lg w-full md:max-w-max bg-white text-center text-base md:text-lg font-semibold text-sky-900">Whatsapp İletişim Hattı</Link>
-                    <Link href="#" className="py-4 px-6 w-full rounded-lg bg-sky-500 md:max-w-max text-base md:text-lg text-center font-semibold text-white">Bizi Arayın</Link>
+                    <Link href="https://wa.me/905434718834?text=Merhaba,bilgi almak istiyorum." className="py-4 px-6 rounded-lg w-full md:max-w-max bg-white text-center text-base md:text-lg font-semibold text-sky-900">Whatsapp İletişim Hattı</Link>
+                    <Link href="905434718834" className="py-4 px-6 w-full rounded-lg bg-sky-500 md:max-w-max text-base md:text-lg text-center font-semibold text-white">Bizi Arayın</Link>
                 </div>
             </div>
         </section>
