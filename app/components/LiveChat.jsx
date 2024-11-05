706123ab610ff3d4b662b8248ad3672d45827a8b
Doganay KOSEOGLU <81515348+dkoode@users.noreply.github.com>
2024-11-05 19:18:30 +0300

    chat update

--- app/components/LiveChat.jsx
+++ app/components/LiveChat.jsx
@@ -3,8 +3,8 @@ import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 export default function LiveChat() {
     return (
         <TawkMessengerReact
-            propertyId="66fed693256fb1049b1c7f3e"
-            widgetId="1i99nccdt"
+            propertyId="672a3ead2480f5b4f5990bba"
+            widgetId="1ibug9m2e"
         />
     )
 }
